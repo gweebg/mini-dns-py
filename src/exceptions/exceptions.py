fb6c077bc8159baa5fe913be4b3ef6adf0c199a7
Guilherme <guilherme.g.sampaio@gmail.com>
2022-10-23 21:56:28 +0100

    Using dict of methods to make parsing easier and faster.
    Only two parsing function implemented for database, missing 7 more :)

--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -5,3 +5,12 @@ class InvalidConfigFileException(Exception):
 
     def __init__(self, message='Bad configuration file.'):
         super(InvalidConfigFileException, self).__init__(message)
+
+
+class InvalidDatabaseFileException(Exception):
+    """
+    Custom exception for invalid parameters.
+    """
+
+    def __init__(self, message='Bad database file.'):
+        super(InvalidDatabaseFileException, self).__init__(message)
