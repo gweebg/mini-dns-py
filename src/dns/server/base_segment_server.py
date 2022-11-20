3f1c1311bb6fdd5c9b4403144ca5e8e1fb0e0988
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2022-11-20 18:34:58 +0000

    Documentation for BaseSegementServer.

--- src/dns/server/base_segment_server.py
+++ src/dns/server/base_segment_server.py
@@ -4,8 +4,20 @@ import logging
 
 
 class BaseSegmentServer:
+    """
+    Class that represent a simple and basic TCP server.
+    The method BaseSegmentServer::tcp_handle() must be overwritten by a subclass of BaseSegmentServer.
+    Use the method BaseSegmentServer::tcp_start() to run the server.
+    """
 
     def __init__(self, ip_address: str, port: int, read_size: int = 1024):
+        """
+        BaseSegmentServer constructor.
+
+        :param ip_address: IP Address to listen to.
+        :param port: Port to listen to.
+        :param read_size: Amount of bytes read from the socket at a time.
+        """
 
         self.tcp_socket_address: tuple[str, int] = (ip_address, port)
         self.tcp_read_size = read_size
@@ -23,13 +35,23 @@ class BaseSegmentServer:
             raise
 
     def tcp_handle(self, conn: socket, address: tuple[str, int]):
+        """
+        This function must be overwritten to actually do something. Its purpose is to handle the incoming data
+        read on the BaseDatagramServer::udp_start() method.
 
-        message = conn.recv(self.tcp_read_size).decode('ascii')
-        conn.send(message.encode('ascii'))
-
-        conn.close()
+        :param conn: 'socket' object that can be used to receive data.
+        :param address: (IP,PORT) tuple of the sender.
+        :return: None
+        """
+        pass
 
     def tcp_start(self):
+        """
+        This function starts the listening process, on an infinite loop it listens to the specified ip address and port
+        and for each message received it creates a processing thread that runs BaseSegmentServer::tcp_handle().
+
+        :return: None
+        """
 
         self.tcp_logger.info(f'EV | {self.tcp_socket_address[0]} | TCP Server is listening on {self.tcp_socket_address[0]}:{self.tcp_socket_address[1]}')
 
@@ -42,9 +64,3 @@ class BaseSegmentServer:
             thread.start()
 
             self.tcp_logger.info(f'EV | {self.tcp_socket_address[0]} | TCP Active connections: {threading.active_count() - 1}')
-
-
-# if __name__ == '__main__':
-#
-#     x = BaseSegmentServer('localhost', 20001)
-#     x.tcp_start()
