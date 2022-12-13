0451e70cf57f488d02a8e005392137c18f999c6f
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2022-12-13 15:18:46 +0000

    Iterative resolution almost done.
    Only missing the Todo's!

--- src/dns/server/resolution_server.py
+++ src/dns/server/resolution_server.py
@@ -1,9 +1,13 @@
+import socket
+from typing import Optional
+
 from common.logger.logger import Logger
-from dns.dns_packet import DNSPacket, DNSPacketHeaderFlag
+from dns.dns_packet import DNSPacket, DNSPacketHeaderFlag, DNSPacketQueryInfo
 from dns.server.base_datagram_server import BaseDatagramServer
 from dns.server.server_config import ServerConfiguration
 
 from exceptions.exceptions import InvalidDNSPacket
+from models.dns_resource import DNSValueType, DNSResource
 
 from parser.abstract_parser import Mode
 from parser.parser_factory import FileParserFactory
@@ -37,12 +41,66 @@ class ResolutionServer(BaseDatagramServer, Logger):
         self.root_servers: list[str] = FileParserFactory(self.configuration.root_servers_path,
                                                          Mode.RT).get_parser().parse()
 
+        # Storing the timeout value for later use while realying the message.
+        self.timeout = timeout
+
         # Todo #
         # Loading database/configuration values into cache.
         # self.cache = Cache()
         # self.cache.from_configuration(self.configuration, "R")
 
-    def relay(self, packet: DNSPacket, address: str):
+    @staticmethod
+    def get_next_address(received_packet: DNSPacket, domain_name: str) -> Optional[str]:
+        """
+        The get_next_address method is used to determine the next address we will be contacting.
+        At this point we already know that there will only exist values on authority values and extra values.
+        We need to get the 'longest' suffix match out of every authority value, not forgetting to check and replace
+        the name if there's a CNAME entry for any authority, and then retrive its corresponding address from
+        the extra values.
+
+        :param received_packet: The answer obtained from one of the servers, where we will look for.
+        :param domain_name: The domain name we want and need to match.
+        :return: Returns the address that matched the longest suffix.
+        """
+
+        matched_authority: Optional[DNSResource] = None
+        closest_index: int = -1
+
+        # Let's first check if we can find the match without having to replace any CNAME entry.
+        for auth_entry in received_packet.query_data.authorities_values:
+
+            # Converted the string entry to a DNSResource object, for the ease of use.
+            entry = DNSResource.from_string(auth_entry)
+
+            # Here we check if the entry parameter is a substring for the domain.
+            if entry.type == DNSValueType.NS and entry.parameter in domain_name:
+
+                # If it is, we check if it is the closest substring of the authority values.
+                if domain_name.index(entry.parameter) > closest_index:
+                    matched_authority = entry
+
+        # If there's still no match, then we will need to look for the CNAME entries.
+        if matched_authority is None:
+            # Todo, replace CNAME's like macros. #
+            ...
+
+        # Now that we're sure that we found a match, we will get it address!
+        for extra_value in received_packet.query_data.extra_values:
+
+            # Parsing the string value into a DNSResource.
+            extra_entry = DNSResource.from_string(extra_value)
+
+            # Matching the resource value and the extra value parameter to check if we got the correct address.
+            if matched_authority.value == extra_entry.parameter:
+
+                # This will be the next address we will be realying the packet to!
+                return extra_entry.value
+
+        # This function should never return None, if it does, then the server that created 'received_packet'
+        # is not well-built!
+        return None
+
+    def relay(self, packet: DNSPacket, address: str) -> Optional[DNSPacket]:
         """
         This method is responsible for obtaining the query result by
         relaying possibly multiple times the query through the servers.
@@ -55,13 +113,21 @@ class ResolutionServer(BaseDatagramServer, Logger):
         # While the answer we receive is not final, we will keep trying.
         while DNSPacketHeaderFlag.F not in packet.header.flags:
 
-            # Relaying the packet to the intendend server at 'relay_ip_address'.
-            self.udp_socket.sendto(packet.as_byte_string(), address)
+            # We need to relay the message to another socket, else there will
+            # be conflict with the main listening thread.
+            relay_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-            # Todo #
-            # Send to another socket, else there will be conflict. #
+            # Setting the same timeout value as for the normal queries.
+            relay_socket.settimeout(self.timeout)
+
+            # Binding the socket to the given address and to a random available port (value 0 does that).
+            relay_socket.bind((address, 0))
+
+            # Relaying the packet to the intendend server at 'relay_ip_address'.
+            relay_socket.sendto(packet.as_byte_string(), address)
+
             # Waiting, receving, decoding and parsing the response.
-            data: str = self.udp_socket.recvfrom(self.read_size).decode('utf-8')
+            data: str = relay_socket.recv(self.read_size).decode('utf-8')
 
             try:
                 received_packet: DNSPacket = DNSPacket.from_string(data)
@@ -76,7 +142,18 @@ class ResolutionServer(BaseDatagramServer, Logger):
                 return received_packet
 
             # Ouch, it was not final, let's relay it to somewhere else specified on the received packet!
-            # Todo #
+
+            # First of all, let's check the response code, if it is 2 the domain does not exist.
+            # The packet may come as an error message, but we still need to communicate it to the client.
+            if received_packet.header.response_code in [2, 3]:
+                return received_packet
+
+            # Now, let's update our address to the one indicated on the received_packet.
+            # To do this, let's use the method 'get_next_address'.
+
+            address = self.get_next_address(received_packet, packet.query_info.name)
+
+            relay_socket.close()
 
     def udp_handle(self, data: bytes, address: tuple[str, int]):
         """
